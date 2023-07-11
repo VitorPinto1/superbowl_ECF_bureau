69c55c0142736bf9a530eed66a51ee3443b23b67
Vitor Pinto Canadas <vitorpinto@mbp-de-vitor.home>
2023-07-11 22:49:34 +0200

    interface commentaires

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from tkinter import ttk
 from datetime import date
 from ttkthemes import ThemedTk
 
-# Configuración de la base de datos
+# Configuration de la base de données
 config = {
     'host': 'localhost',
     'port': 3306,
@@ -13,126 +13,162 @@ config = {
     'database': 'bdsuperbowl'
 }
 
-# Función para obtener los datos de la tabla "matchs"
+# Variable para almacenar el ID del partido seleccionado
+id_match_selection = None
+
+# Fonction pour obtenir les données de la table "matchs"
 def obtenir_donnees_du_jour():
-    # Conexión a la base de datos
+    # Connexion à la base de données
     conn = mysql.connector.connect(**config)
     cursor = conn.cursor()
 
-    # Obtener fecha actual
+    # Obtenir la date actuelle
     date_actuelle = date.today()
 
-    # Ejecutar consulta
+    # Exécution de la requête
     query = "SELECT id, equipe1, equipe2, jour, debut, fin FROM matchs WHERE jour = %s"
     cursor.execute(query, (date_actuelle,))
 
-    # Obtener los resultados
+    # Obtenir les résultats
     results = cursor.fetchall()
 
-    # Cerrar el cursor y la conexión
+    # Fermeture du curseur et de la connexion
     cursor.close()
     conn.close()
 
     return results
 
-# Función para obtener los datos de apuestas para un partido específico
+# Fonction pour obtenir les données de mise pour un match spécifique
 def obtenir_mises(id_match):
-    # Conexión a la base de datos
+    # Connexion à la base de données
     conn = mysql.connector.connect(**config)
     cursor = conn.cursor()
 
-    # Ejecutar consulta
+    # Exécution de la requête
     query = "SELECT cote1, cote2, id_utilisateur FROM mises WHERE id_match = %s"
     cursor.execute(query, (id_match,))
 
-    # Obtener los resultados
+    # Obtenir les résultats
     results = cursor.fetchall()
 
-    # Cerrar el cursor y la conexión
+    # Fermeture du curseur et de la connexion
     cursor.close()
     conn.close()
 
     return results
 
-# Función para guardar los comentarios y el score de un partido
+# Fonction pour enregistrer les commentaires et le score d'un match
 def commentaires_et_score(id_match, commentaires, score):
-    # Conexión a la base de datos
+    # Connexion à la base de données
     conn = mysql.connector.connect(**config)
     cursor = conn.cursor()
 
-    # Actualizar el partido con los comentarios y el score
+    # Mise à jour du match avec les commentaires et le score
     query = "UPDATE matchs SET commentaires = %s, score = %s WHERE id = %s"
     cursor.execute(query, (commentaires, score, id_match))
 
-    # Guardar los cambios en la base de datos
+    # Enregistrer les modifications dans la base de données
     conn.commit()
 
-    # Cerrar el cursor y la conexión
+    # Fermeture du curseur et de la connexion
     cursor.close()
     conn.close()
 
-# Función para manejar la selección de un partido
-def seleccionar_partido(event):
-    item = table_matchs.selection()[0]
-    id_match = table_matchs.item(item, 'text')
-    values = table_matchs.item(item, 'values')
+# Fonction pour gérer la sélection d'un match
+def selectionner_match(event):
+    global id_match_selection
+    if id_match_selection is None:
+        item = table_matchs.selection()[0]
+        id_match = table_matchs.item(item, 'text')
+        values = table_matchs.item(item, 'values')
 
-    # Obtener los datos de apuestas para el partido seleccionado
-    donnees_mises = obtenir_mises(id_match)
+        # Obtenir les données de mise pour le match sélectionné
+        donnees_mises = obtenir_mises(id_match)
 
-    # Imprimir los datos de apuestas
-    print("Partido seleccionado:", values)
-    for donnees in donnees_mises:
-        cote1, cote2, id_utilisateur = donnees
-        print("Cote 1:", cote1)
-        print("Cote 2:", cote2)
-        print("ID utilisateur:", id_utilisateur)
+        # Afficher les données de mise
+        print("Match sélectionné:", values)
+        for donnees in donnees_mises:
+            cote1, cote2, id_utilisateur = donnees
+            print("Cote 1:", cote1)
+            print("Cote 2:", cote2)
+            print("ID utilisateur:", id_utilisateur)
 
-    # Pedir comentarios y score al usuario
-    commentaires = input("Ingrese los comentarios del partido: ")
-    score = input("Ingrese el score del partido: ")
+        # Enregistrer l'ID du match sélectionné
+        id_match_selection = id_match
 
-    # Guardar los comentarios y el score en la base de datos
-    commentaires_et_score(id_match, commentaires, score)
+        # Créer une nouvelle fenêtre pour saisir les commentaires et le score
+        frame_inputs = ttk.Frame(fenetre)
+        frame_inputs.pack(pady=10)
 
-# Crear ventana de aplicación
+        # Étiquette et champ de saisie pour les commentaires
+        lbl_commentaires = tk.Label(frame_inputs, text="Commentaires:")
+        lbl_commentaires.grid(row=0, column=0, sticky=tk.E)
+        entry_commentaires = tk.Entry(frame_inputs)
+        entry_commentaires.grid(row=0, column=1)
+
+        # Étiquette et champ de saisie pour le score
+        lbl_score = tk.Label(frame_inputs, text="Score:")
+        lbl_score.grid(row=1, column=0, sticky=tk.E)
+        entry_score = tk.Entry(frame_inputs)
+        entry_score.grid(row=1, column=1)
+
+        # Fonction pour enregistrer les commentaires et le score en appuyant sur un bouton
+        def enregistrer_commentaires_et_score():
+            commentaires = entry_commentaires.get()
+            score = entry_score.get()
+
+            # Enregistrer les commentaires et le score dans la base de données
+            commentaires_et_score(id_match, commentaires, score)
+
+            # Réinitialiser l'ID du match sélectionné
+            id_match_selection = None
+
+            # Détruire le frame_inputs
+            frame_inputs.destroy()
+
+            
+        # Bouton pour enregistrer les commentaires et le score
+        btn_enregistrer = tk.Button(fenetre, text="Enregistrer", command=enregistrer_commentaires_et_score)
+        btn_enregistrer.pack()
+
+# Créer la fenêtre de l'application
 fenetre = ThemedTk(theme="default")
 fenetre.title("Matchs")
 
-# Crear árbol de datos con estilo mejorado
+# Créer un arbre de données avec un style amélioré
 style = ttk.Style(fenetre)
 style.configure("Custom.Treeview", highlightthickness=0, bd=0)
 style.configure("Custom.Treeview.Heading", font=('Helvetica', 10, 'bold'))
-table_matchs = ttk.Treeview(fenetre, columns=("equipo1", "equipo2", "jour", "debut", "fin"), style="Custom.Treeview")
-table_matchs["columns"] = ("equipo1", "equipo2", "jour", "debut", "fin")
+table_matchs = ttk.Treeview(fenetre, columns=("equipe1", "equipe2", "jour", "debut", "fin"), style="Custom.Treeview")
+table_matchs["columns"] = ("equipe1", "equipe2", "jour", "debut", "fin")
 
 table_matchs["show"] = "headings"
 
-# Desactivar redimensionamiento de columnas
-table_matchs.bind("<Button-1>", seleccionar_partido)
+# Désactiver le redimensionnement des colonnes
+table_matchs.bind("<Button-1>", selectionner_match)
 
-# Definir encabezados de columnas
-table_matchs.heading("equipo1", text="Equipe 1")
-table_matchs.heading("equipo2", text="Equipe 2")
+# Définir les en-têtes des colonnes
+table_matchs.heading("equipe1", text="Équipe 1")
+table_matchs.heading("equipe2", text="Équipe 2")
 table_matchs.heading("jour", text="Jour")
-table_matchs.heading("debut", text="Debut")
+table_matchs.heading("debut", text="Début")
 table_matchs.heading("fin", text="Fin")
 
-# Configurar columnas para que sean fijas y no modificables
-column_widths = {"equipo1": 100, "equipo2": 100, "jour": 100, "debut": 100, "fin": 100}
-for column, width in column_widths.items():
-    table_matchs.column(column, width=width, anchor=tk.CENTER)
+# Configurer les colonnes pour qu'elles soient fixes et non modifiables
+largeurs_colonnes = {"equipe1": 100, "equipe2": 100, "jour": 100, "debut": 100, "fin": 100}
+for colonne, largeur in largeurs_colonnes.items():
+    table_matchs.column(colonne, width=largeur, anchor=tk.CENTER)
 
-# Obtener datos de la tabla "matchs"
-donnees_matchs_actuelles = obtenir_donnees_du_jour()
+# Obtenir les données de la table "matchs"
+donnees_matchs_actuels = obtenir_donnees_du_jour()
 
-# Insertar los datos en el árbol de datos
-for row in donnees_matchs_actuelles:
-    partido_id, *values = row
-    table_matchs.insert("", tk.END, text=partido_id, values=values[:5])  # 5 valores
+# Insérer les données dans l'arbre de données
+for row in donnees_matchs_actuels:
+    id_match, *values = row
+    table_matchs.insert("", tk.END, text=id_match, values=values[:5])  # 5 valeurs
 
-# Empacar el árbol de datos
+# Pack the treeview
 table_matchs.pack()
 
-# Iniciar bucle de eventos de la aplicación
+# Démarrer la boucle d'événements de l'application
 fenetre.mainloop()
