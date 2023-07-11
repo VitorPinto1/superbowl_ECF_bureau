eb404ba37ff0f72f9c21eadaa3a70f1098314dea
Vitor Pinto Canadas <vitorpinto@mbp-de-vitor.home>
2023-07-11 22:23:33 +0200

    selection-matchs

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import mysql.connector
 import tkinter as tk
 from tkinter import ttk
@@ -14,18 +13,17 @@ config = {
     'database': 'bdsuperbowl'
 }
 
-
 # Función para obtener los datos de la tabla "matchs"
 def obtenir_donnees_du_jour():
     # Conexión a la base de datos
     conn = mysql.connector.connect(**config)
     cursor = conn.cursor()
 
-    # obtenir date
+    # Obtener fecha actual
     date_actuelle = date.today()
 
     # Ejecutar consulta
-    query = "SELECT equipe1, equipe2, jour, debut, fin FROM matchs WHERE jour = %s"
+    query = "SELECT id, equipe1, equipe2, jour, debut, fin FROM matchs WHERE jour = %s"
     cursor.execute(query, (date_actuelle,))
 
     # Obtener los resultados
@@ -36,6 +34,66 @@ def obtenir_donnees_du_jour():
     conn.close()
 
     return results
+
+# Función para obtener los datos de apuestas para un partido específico
+def obtenir_mises(id_match):
+    # Conexión a la base de datos
+    conn = mysql.connector.connect(**config)
+    cursor = conn.cursor()
+
+    # Ejecutar consulta
+    query = "SELECT cote1, cote2, id_utilisateur FROM mises WHERE id_match = %s"
+    cursor.execute(query, (id_match,))
+
+    # Obtener los resultados
+    results = cursor.fetchall()
+
+    # Cerrar el cursor y la conexión
+    cursor.close()
+    conn.close()
+
+    return results
+
+# Función para guardar los comentarios y el score de un partido
+def commentaires_et_score(id_match, commentaires, score):
+    # Conexión a la base de datos
+    conn = mysql.connector.connect(**config)
+    cursor = conn.cursor()
+
+    # Actualizar el partido con los comentarios y el score
+    query = "UPDATE matchs SET commentaires = %s, score = %s WHERE id = %s"
+    cursor.execute(query, (commentaires, score, id_match))
+
+    # Guardar los cambios en la base de datos
+    conn.commit()
+
+    # Cerrar el cursor y la conexión
+    cursor.close()
+    conn.close()
+
+# Función para manejar la selección de un partido
+def seleccionar_partido(event):
+    item = table_matchs.selection()[0]
+    id_match = table_matchs.item(item, 'text')
+    values = table_matchs.item(item, 'values')
+
+    # Obtener los datos de apuestas para el partido seleccionado
+    donnees_mises = obtenir_mises(id_match)
+
+    # Imprimir los datos de apuestas
+    print("Partido seleccionado:", values)
+    for donnees in donnees_mises:
+        cote1, cote2, id_utilisateur = donnees
+        print("Cote 1:", cote1)
+        print("Cote 2:", cote2)
+        print("ID utilisateur:", id_utilisateur)
+
+    # Pedir comentarios y score al usuario
+    commentaires = input("Ingrese los comentarios del partido: ")
+    score = input("Ingrese el score del partido: ")
+
+    # Guardar los comentarios y el score en la base de datos
+    commentaires_et_score(id_match, commentaires, score)
 
 # Crear ventana de aplicación
 fenetre = ThemedTk(theme="default")
@@ -51,7 +109,7 @@ table_matchs["columns"] = ("equipo1", "equipo2", "jour", "debut", "fin")
 table_matchs["show"] = "headings"
 
 # Desactivar redimensionamiento de columnas
-table_matchs.bind("<Button-1>", lambda e: "break")
+table_matchs.bind("<Button-1>", seleccionar_partido)
 
 # Definir encabezados de columnas
 table_matchs.heading("equipo1", text="Equipe 1")
@@ -60,20 +118,18 @@ table_matchs.heading("jour", text="Jour")
 table_matchs.heading("debut", text="Debut")
 table_matchs.heading("fin", text="Fin")
 
-
 # Configurar columnas para que sean fijas y no modificables
 column_widths = {"equipo1": 100, "equipo2": 100, "jour": 100, "debut": 100, "fin": 100}
 for column, width in column_widths.items():
     table_matchs.column(column, width=width, anchor=tk.CENTER)
-
-
 
 # Obtener datos de la tabla "matchs"
 donnees_matchs_actuelles = obtenir_donnees_du_jour()
 
 # Insertar los datos en el árbol de datos
 for row in donnees_matchs_actuelles:
-    table_matchs.insert("", tk.END, values=row[:5]) # 5 valeurs
+    partido_id, *values = row
+    table_matchs.insert("", tk.END, text=partido_id, values=values[:5])  # 5 valores
 
 # Empacar el árbol de datos
 table_matchs.pack()
